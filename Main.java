6ea9727931667887180bff86e3223d076ddfefcb
joseantonio_or <joseanpolixe@gmail.com>
2024-11-18 17:10:52 +0100

    More code written in the file...

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         // Crear tabla y llenarla con valores aleatorios
@@ -6,6 +7,12 @@ public class Main {
         Random random = new Random();
         for (int i = 0; i < table.length; i++) {
             table[i] = random.nextInt(100); // Valores entre 0 y 99
+        }
+
+        // Imprimir los valores de la tabla
+        System.out.println("Table elements:");
+        for (int value : table) {
+            System.out.println(value);
         }
     }
 }
