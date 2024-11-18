47cfd15346ce218baf992999ad2c87bb5a7b77ca
joseantonio_or <joseanpolixe@gmail.com>
2024-11-18 17:26:11 +0100

    Third code inserted, sort the table...

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,6 +16,10 @@ public class Main {
             System.out.println(value);
         }
 
+        Arrays.sort(table);
+
+        System.out.println("Sorted table: ");
+        System.out.println(Arrays.toString(table));
 
     }
 }
