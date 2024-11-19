6456dd6fac1b04b7451b7da47fb4797803f0b8bc
joseantonio_or <joseanpolixe@gmail.com>
2024-11-19 14:38:58 +0100

    Changing sorting algorithm to insertion sort

--- Main.java
+++ Main.java
@@ -15,13 +15,37 @@ public class Main {
         for (int value : table) {
             System.out.println(value);
         }
-
+        /*
         Arrays.sort(table);
 
         System.out.println("Sorted table: ");
         System.out.println(Arrays.toString(table));
 
         System.out.println("This is more code for the point ");
+        */
+        System.out.println("Original array: " + Arrays.toString(table));
 
+        insertionSort(table);
+
+        System.out.println("Sorted array (Insertion Sort): " + Arrays.toString(table));
     }
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Move elements of arr[0..i-1], that are greater than key,
+            // to one position ahead of their current position
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+
+
 }
